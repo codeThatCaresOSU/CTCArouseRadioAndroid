4ac0eb45126af911e4af1f16fecb15381d5035bb
Erik Gustafson <erikgustafson09@gmail.com>
2019-09-09 19:09:19 -0400

    app now plays an mp3 file on startup

--- app/src/main/java/com/codethatcares/arouseradio/MusicPlayerFragment.java
+++ app/src/main/java/com/codethatcares/arouseradio/MusicPlayerFragment.java
@@ -1,20 +1,31 @@
 package com.codethatcares.arouseradio;
 
+import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class MusicPlayerFragment extends Fragment {
 
+    private MediaPlayer mediaPlayer;
+
+
     //do stuff with data
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        new Player().execute("https://file-examples.com/wp-content/uploads/2017/11/file_example_MP3_700KB.mp3");
     }
 
     //do stuff with views
@@ -23,5 +34,34 @@ public class MusicPlayerFragment extends Fragment {
         View v = inflater.inflate(R.layout.music_player_fragment, container, false);
 
         return v;
+    }
+
+    class Player extends AsyncTask<String, Void, Boolean> {
+        @Override
+        protected Boolean doInBackground(String... args) {
+            Boolean prepared;
+            try {
+                mediaPlayer.setDataSource(args[0]);
+                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mediaPlayer) {
+                        Log.e("test", "on complete");
+                        mediaPlayer.stop();
+                        mediaPlayer.reset();
+                    }
+                });
+                mediaPlayer.prepare();
+                prepared = true;
+            } catch (Exception e) {
+                e.printStackTrace();
+                prepared = false;
+            }
+            return prepared;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+            mediaPlayer.start();
+        }
     }
 }
