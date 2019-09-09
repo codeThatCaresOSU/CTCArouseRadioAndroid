2a24c728b3b6ce0ac5d57ebf050db9b9bead56ea
Chris <chris4lilv@gmail.com>
2019-09-09 18:17:22 -0400

    Intial UI

--- app/src/main/java/com/codethatcares/arouseradio/MainActivity.kt
+++ app/src/main/java/com/codethatcares/arouseradio/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.codethatcares.arouseradio
 
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.graphics.drawable.RoundedBitmapDrawable
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
+
     }
 }
