cd384c62ded9905a02e17ee6e69e1f5b9da1f4c6
Chris <chris4lilv@gmail.com>
2019-09-09 19:15:47 -0400

    Crop cover to circle

--- app/src/main/java/com/codethatcares/arouseradio/MusicPlayerFragment.java
+++ app/src/main/java/com/codethatcares/arouseradio/MusicPlayerFragment.java
@@ -1,13 +1,17 @@
 package com.codethatcares.arouseradio;
 
+import android.graphics.*;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import com.bumptech.glide.Glide;
 
 public class MusicPlayerFragment extends Fragment {
 
@@ -16,12 +20,64 @@ public class MusicPlayerFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
+    private ImageView imgView;
+    private CardView cardView;
 
     //do stuff with views
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.music_player_fragment, container, false);
 
+        imgView = v.findViewById(R.id.cover_album);
+        cardView = v.findViewById(R.id.cover_card_view);
+
+        Bitmap originBitmap = Bitmap.createBitmap(BitmapFactory.decodeResource(getResources(), R
+                .drawable.samplecover));
+
+        Bitmap bitmap = getCircleBitmap(originBitmap);
+        //let bitmap fit parent
+        imgView.setScaleType(ImageView.ScaleType.FIT_XY);
+
+        Glide.with(this).load(bitmap).into(imgView);
+
         return v;
+    }
+
+    public static Bitmap getCircleBitmap(Bitmap bitmap) {
+        if (bitmap == null) {
+            return null;
+        }
+        bitmap = cropBitmap(bitmap);
+        try {
+            Bitmap circleBitmap = Bitmap.createBitmap(bitmap.getWidth(),
+                    bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+            Canvas canvas = new Canvas(circleBitmap);
+            final Paint paint = new Paint();
+            final Rect rect = new Rect(0, 0, bitmap.getWidth(),
+                    bitmap.getHeight());
+            final RectF rectF = new RectF(new Rect(0, 0, bitmap.getWidth(),
+                    bitmap.getHeight()));
+            float roundPx = 0.0f;
+            roundPx = bitmap.getWidth();
+            paint.setAntiAlias(true);
+            canvas.drawARGB(0, 0, 0, 0);
+            paint.setColor(Color.WHITE);
+            canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
+            paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+            final Rect src = new Rect(0, 0, bitmap.getWidth(),
+                    bitmap.getHeight());
+            canvas.drawBitmap(bitmap, src, rect, paint);
+            return circleBitmap;
+        } catch (Exception e) {
+            return bitmap;
+        }
+    }
+
+    public static Bitmap cropBitmap(Bitmap bitmap) {
+        int w = bitmap.getWidth();
+        int h = bitmap.getHeight();
+        int cropWidth = w >= h ? h : w;
+        return Bitmap.createBitmap(bitmap, (bitmap.getWidth() - cropWidth) / 2,
+                (bitmap.getHeight() - cropWidth) / 2, cropWidth, cropWidth);
     }
 }
